d5aa87baa1fa2265ad652db192ecb40f831fe094
MrTartuf0 <79630556+MrTartuf0@users.noreply.github.com>
2022-07-15 10:11:18 +0200

    icon

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ let stringQR = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=ht
 let characters = [ "0" , "1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "A" , "B" , "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z" ] 
 
 for(let j=0 ; j<100 ; j++){
-    let stringCode = ""
-    for(let i=0 ; i<6 ; i++){
+    let stringCode = "H"
+    for(let i=0 ; i<5 ; i++){
         let singleChar = characters[Math.floor(Math.random() * 36)]
         stringCode += singleChar
     }
